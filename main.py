1ca66ab3362031463a9132bf09d562dfffc05051
Aissata GUINDO <153300440+GuindoAissata@users.noreply.github.com>
2025-04-06 17:25:51 +0200

    Update main.py
    cleaning

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def generer_villes():
         x2,y2 = villes [i+1]
         canvas.create_line(x1,y1,x2,y2, fill ="black",width=2,tags="ligne")#liaison entre les villes
 
-def dessiner_villes(villes):
+def dessiner_villes(villes):#cette fonction sera utilisée dans run_algo pour dessiner les villes et chemins 
     canvas_resultat.delete("all") #reinitialisé la fenetre 
     for i in range (len(villes)-1):
         x1,y1 = villes[i]
@@ -37,54 +37,14 @@ def dessiner_villes(villes):
     canvas_resultat.create_line(x1, y1, x2, y2, fill="red", width=2, tags="ligne")
     canvas_resultat.create_oval(x1-5, y1-5, x1+5, y1+5, fill= "red",tags="ligne")
 
-
-    #return villes #on utilisera la ville retournée pour paramètre de créer_population
-
-
 def run_algo():
     taille_pop = 100
-
-
+    
     best_way ,best_fit = genetique(villes,taille_pop)
     print(f"le meilleur chemin trouvé est : {best_way} ")
     print(f"la meilleure distance trouvée est : {best_fit} ")
-
-
+    
     return best_way, best_fit ,dessiner_villes(best_way), label_distance.config(text=f"BEST DIST : {best_fit :.2f}")
-
-
-
-
-
-###########TEST DE creer_population #####################
-# Liste de villes (coordonnées fictives)
-villes_test = [(100, 200), (250, 400), (50, 50), (300, 150)]
-
-# Taille de la population à générer
-taille_population_test = 5
-
-# Appel de la fonction
-population_generee = creer_population(villes_test, taille_population_test)
-
-parent1,parent2 = selection(population_generee,3)
-enfant_i = crossover(parent1,parent2)
-enfant_f= mutation(enfant_i)
-nouvelle_pop = formation(population_generee,enfant_i)
-best_way, best_fit = genetique(villes_test,taille_population_test)
-
-# Affichage des résultats
-print("Population générée :")
-for i, individu in enumerate(population_generee):
-    print(f"Itinéraire {i+1} : {individu} fitness{i+1} : {fitness(individu)}")
-
-print (f"parents séletionnes :  {parent1} , {parent2}")
-print (f"un enfant des parents sélectionnés est : {enfant_i} ")
-print (f"un enfant des parents sélectionnés est : {enfant_f} ")
-print (f"La nouvelle population est : {nouvelle_pop}")
-print (f"le meilleur chemin est {best_way}")
-print (f"la meilleure distance trouvée {best_fit}")
-
-
 
 # fenetre 
 fenetre = tk.Tk()
@@ -133,10 +93,6 @@ bouton2.pack(pady=5)
 
 label_distance = tk.Label(frame_genetic,text="BEST DISTANCE : ")
 label_distance.pack()
-
-
-
-
 
 # Lancer l'application
 fenetre.mainloop()
